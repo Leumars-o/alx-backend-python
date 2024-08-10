e1014ac740791151cc1580b385abeefb7907a11f
leumars-o <samex150@gmail.com>
2024-08-10 03:41:24 +0100

    executing multiple coroutines output syntax modified

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,10 +2,11 @@
 """This module executes multiple coroutines at the same time.
 """
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """This coroutine will execute multiple coroutines at the same time with
     a specific delay.
 
