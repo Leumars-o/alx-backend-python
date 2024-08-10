7bae47252a4d680e583f34698d45b57e78b3f04e
leumars-o <samex150@gmail.com>
2024-08-10 04:19:02 +0100

    async comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""This module contains an Async comprehension function
+"""
+
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    """This function collects 10 random numbers using an async generator
+    """
+    return [i async for i in async_generator()]
